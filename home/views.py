417da1e7043c8b8c0b4b372239bc1d78ea5ce3c3
sydulamin <sydulamin6@gmail.com>
2025-08-07 18:37:52 +0600

    Fix merge conflicts in urls.py and views.py

--- home/views.py
+++ home/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 1a2fe74802877d6e0e28ce48b891b158fc9cbf42
 def home(request):
     return render(request, 'main.html')
-=======
+
 def store(request):
     return render(request, 'home/store.html')
->>>>>>> 8ac853f... Templates config
+
 
 
 def cart(request):
