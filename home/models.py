7c1091ef6f5ba1adeaa24039a11acb739618abd9
Shafath Mahmud choudhury~ <shafath061@gmail.com>
2025-09-01 13:17:13 +0600

    Added code to save transaction information at the backend

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Order(models.Model):
+    tran_id = models.CharField(max_length=100, unique=True)
+    val_id = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=50)
+    amount = models.FloatField()
+    currency = models.CharField(max_length=10)
+    payment_info = models.JSONField()  # stores full payment info as JSON
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.tran_id} - {self.status}"
